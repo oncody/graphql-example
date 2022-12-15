360f1e42d9efcabf6a8fc6d5012df6821ea0886f
Cody Smith <37386486+oncody@users.noreply.github.com>
2022-12-14 19:21:18 -0700

    adding books on authors

--- server.js
+++ server.js
@@ -46,100 +46,118 @@ let schema = buildSchema(`
   
 `);
 
-let authors = [
-  {
-    id: 0,
-    name: 'author0'
-  },
-  {
-    id: 1,
-    name: 'author1'
-  },
-  {
-    id: 2,
-    name: 'author2'
-  },
-  {
-    id: 3,
-    name: 'author3'
-  },
-];
+let author0 = {
+      id: 0,
+      name: 'author0'
+    };
 
-let author0books = [
-  {
-    id: 4,
-    title: 'author0book0'
-  },
-  {
-    id: 5,
-    title: 'author0book1'
-  },
-  {
-    id: 6,
-    title: 'author0book2'
-  },
-  {
-    id: 7,
-    title: 'author0book3'
-  },
-];
+author0.books =  [
+    {
+      id: 4,
+      title: 'author0book0',
+      author: author0,
+    },
+    {
+      id: 5,
+      title: 'author0book1',
+      author: author0,
+    },
+    {
+      id: 6,
+      title: 'author0book2',
+      author: author0,
+    },
+    {
+      id: 7,
+      title: 'author0book3',
+      author: author0,
+    }]
 
-let author1books = [
+let author1 = {
+      id: 1,
+      name: 'author1'
+    };
+
+author1.books = [
   {
     id: 8,
-    title: 'author1book0'
+    title: 'author1book0',
+    author: author1,
   },
   {
     id: 9,
-    title: 'author1book1'
+    title: 'author1book1',
+    author: author1,
   },
   {
     id: 10,
-    title: 'author1book2'
+    title: 'author1book2',
+    author: author1,
   },
   {
     id: 11,
-    title: 'author1book3'
+    title: 'author1book3',
+    author: author1,
   },
 ];
 
-let author2books = [
+let author2 = {
+      id: 2,
+      name: 'author2'
+    };
+
+author2.books = [
   {
     id: 12,
-    title: 'author2book0'
+    title: 'author2book0',
+    author: author2,
   },
   {
     id: 13,
-    title: 'author2book1'
+    title: 'author2book1',
+    author: author2,
   },
   {
     id: 14,
-    title: 'author2book2'
+    title: 'author2book2',
+    author: author2,
   },
   {
     id: 15,
-    title: 'author2book3'
+    title: 'author2book3',
+    author: author2,
   },
 ];
 
-let author3books = [
+let author3 = {
+      id: 3,
+      name: 'author3'
+    };
+
+author3.books = [
   {
     id: 16,
-    title: 'author3book0'
+    title: 'author3book0',
+    author: author3,
   },
   {
     id: 17,
-    title: 'author3book1'
+    title: 'author3book1',
+    author: author3,
   },
   {
     id: 18,
-    title: 'author3book2'
+    title: 'author3book2',
+    author: author3,
   },
   {
     id: 19,
-    title: 'author3book3'
+    title: 'author3book3',
+    author: author3,
   },
 ];
+
+let authors = [author0, author1, author2, author3];
 
 const LIMIT = 2;
 
